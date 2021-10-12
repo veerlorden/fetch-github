d0c97267d9ea5c911dfa548c497f6613ace7e876
veerlorden <7z95A0dJ73h213>
2021-10-12 12:27:23 +0300

    Initial Commit

---
+++ FetchTest/FetchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTestApp.swift
+//  FetchTest
+//
+//  Created by Вадим Буркин on 12.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct FetchTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
